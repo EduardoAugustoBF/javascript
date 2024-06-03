4f74d183c7daaa41d7b1c3d71bc7c6dc8120ecb2
Eduardo Augusto <163478183+EduardoAugustoBF@users.noreply.github.com>
2024-06-03 23:28:45 +0100

    Exercício

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,7 +1,3 @@
-var vel = 50
-console.log(`Você está dirigindo á ${vel}km/h`)
-if (vel > 60) {
-  console.log('Você está MULTADO por excesso de velocidade!')
-} else {
-  console.log('Parabéns, Você está dirigindo corretamente')
-} console.log('Use sempre o cinto de segurança!')
+for (var c = 1; c>=20; c++) {
+  console.log(c)
+}
