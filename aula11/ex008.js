631883f3cc2eeb1f7b8c11e5a0f5208d8cd1da06
Eduardo Augusto <163478183+EduardoAugustoBF@users.noreply.github.com>
2024-06-03 23:36:17 +0100

    Update ex008.js

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,3 +1,7 @@
-for (var c = 1; c>=20; c++) {
-  console.log(c)
-}
+var vel = 120
+console.log(`Você está dirigndo á ${vel}Km/h`)
+if (vel > 60) {
+  console.log('Você está MULTADO!')
+} else {
+  console.log('Parabéns, você está dirigindo na velocidade correta!')
+} console.log('Dirija sempre usando cinto de segurança!')
