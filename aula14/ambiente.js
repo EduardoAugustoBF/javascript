7d83b011443dab8d6720940e80b63a24c16d7eb5
Eduardo Augusto <163478183+EduardoAugustoBF@users.noreply.github.com>
2024-06-10 16:36:44 +0100

    Estudos

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -1,5 +1,8 @@
 console.log('Vai começar...')
-for (var c = 1; c <= 10; c++) {
-  console.log(c)
+
+for (var c = 1; c<= 20; c++) {
+  console.log(`Passo ${c}`)
 }
 console.log('FIM!')
+
+
--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,4 +1,4 @@
-let num = [5,9,8,3,2]
+let num = [5,8,9,3,6]
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro vetor é ${num[0]}`)
