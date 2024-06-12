b069698743a98caeea8d1cdb839e2ac6a1efaa08
Eduardo Augusto <163478183+EduardoAugustoBF@users.noreply.github.com>
2024-06-12 02:37:32 +0100

    Estudos com JavaScript

--- aula15/vetornatela.js
+++ aula15/vetornatela.js
@@ -1,10 +1,14 @@
-let valores = [5,8,9,6,3]
-console.log(valores)
-
+let num = [5,8,6,9,3]
+console.log(num)
+/*
 for (let pos = 0; pos < valores.length; pos++) {
   console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
-} 
+}*/
 
+/* Jeito mais rápido de fazer */
+for (let pos in num) {
+  console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
 
 
 
