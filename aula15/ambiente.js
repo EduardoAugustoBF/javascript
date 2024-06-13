2fb142121f9c4241e4bcab4c5e8bc5243a1009c4
Eduardo Augusto <163478183+EduardoAugustoBF@users.noreply.github.com>
2024-06-13 01:03:13 +0100

    JavaScript

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,12 +1,18 @@
-let num = [5,8,9,6,3] 
+let num = [5,8,9,3,6]
 num.push(1)
 num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro vetor é ${num[0]}`)
-let pos = num.indexOf(8)
+let pos = num.indexOf(9)
 if (pos == -1) {
   console.log('O valor não foi encontrado!')
 } else {
   console.log(`O valor está na posição ${pos}`)
 }
+
+
+
+
+
+
--- aula15/vetornatela.js
+++ aula15/vetornatela.js
@@ -1,14 +1,18 @@
-let num = [5,8,6,9,3]
-console.log(num)
+
 /*
 for (let pos = 0; pos < valores.length; pos++) {
   console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }*/
 
 /* Jeito mais rápido de fazer */
+let num = [5,8,9,6,3]
+console.log(num)
+
 for (let pos in num) {
   console.log(`A posição ${pos} tem o valor ${num[pos]}`)
 }
+
+
 
 
 
