4022edba048f6567882cf752f25647646ffbe4b0
Eduardo Augusto <163478183+EduardoAugustoBF@users.noreply.github.com>
2024-06-19 00:24:52 +0100

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -4,13 +4,13 @@ num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro vetor é ${num[0]}`)
-let pos = num.indexOf(6)
+let pos = num.indexOf(8)
 if (pos == -1) {
   console.log('O valor não foi encontrado!')
 } else {
   console.log(`O valor está na posição ${pos}`)
 }
-
+ 
 
 
 
